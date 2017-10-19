22dffd34a8547ed8f2ec6a966ab897f30c1d46ce
etalbot1291 <e.talbot@liverpool.ac.uk>
2017-10-19 10:10:00 +0100

    Update TwitterDataDL.R
    Added Twitter API authentication code.

--- TwitterDataDL.R
+++ TwitterDataDL.R
@@ -1,3 +1,16 @@
+
+install.packages("twitteR")
+library("twitteR")
+# Go to https://dev.twitter.com/oauth/overview/application-owner-access-tokens
+# and get the access and consumer token/secret (you will need a Twitter Account to do this).
+consumer_key <- ''
+consumer_secret <- ''
+access_token <- ''
+access_secret <- ''
+
+#TwitteR authentication for each session - unique to each user
+setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
+
 # Code Source: https://stackoverflow.com/questions/36173472/twitter-how-to-search-for-two-hashtags
 
 hashtags <- c("#MeToo", "#metoo")
