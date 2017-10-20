131a6700e3be4ec97e9af80a04b34256f93dc31b
Azadeh M.Gishani <azadeh@Azadehs-Air.lan>
2017-10-20 13:04:52 +0100

    SearchTwitter Options

--- TwitterDataDL.R
+++ TwitterDataDL.R
@@ -1,12 +1,14 @@
 
 install.packages("twitteR")
 library("twitteR")
-# Go to https://dev.twitter.com/oauth/overview/application-owner-access-tokens
+# Help on API & Tokens: https://dev.twitter.com/oauth/overview/application-owner-access-tokens
+# To register your own API go to https://apps.twitter.com
 # and get the access and consumer token/secret (you will need a Twitter Account to do this).
-consumer_key <- ''
-consumer_secret <- ''
-access_token <- ''
-access_secret <- ''
+# 
+consumer_key <- Omitted1
+consumer_secret <- Omitted2
+access_token <- Omitted3
+access_secret <- Omitted4
 
 #TwitteR authentication for each session - unique to each user
 setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
@@ -15,8 +17,7 @@ setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
 
 hashtags <- c("#MeToo", "#metoo")
 needle <- paste(hashtags, collapse = " OR ")
-tweets <- searchTwitter(needle, n = 10)
-df <- twListToDF(tweets)
-for (hashtag in hashtags) {
-        write.csv(df[grep(hashtag, tolower(df$text), fixed = TRUE), ], paste0(hashtag, ".csv"))
-}
+tweets <- searchTwitter(needle, n = 5000)
+tweets <- twListToDF(tweets)
+
+names(tweets)
